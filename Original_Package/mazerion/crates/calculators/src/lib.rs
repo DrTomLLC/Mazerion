fb8e5a18118e2398deb6bd11900352ead777059e
DrTomLLC <49105299+DrTomLLC@users.noreply.github.com>
2025-11-23 14:54:20 -0500

    Delete Original_Package directory

--- Original_Package/mazerion/crates/calculators/src/lib.rs
+++
@@ -1,9 +0,0 @@
-//! Calculator implementations for Mazerion.
-
-pub mod abv;
-pub mod brix_to_sg;
-pub mod sg_correction;
-
-pub use abv::AbvCalculator;
-pub use brix_to_sg::BrixToSgCalculator;
-pub use sg_correction::SgCorrectionCalculator;
